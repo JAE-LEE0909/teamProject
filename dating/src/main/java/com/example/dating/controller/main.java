7b59f4ded4f21200c51b60732289f9c689e74396
JAE-LEE0909 <kkao456178@gmail.com>
2025-12-08 21:55:49 +0900

    리액트 설정
    설정 중간과정

--- dating/src/main/java/com/example/dating/controller/main.java
+++ dating/src/main/java/com/example/dating/controller/main.java
@@ -1,15 +1,23 @@
-package com.example.dating;
+package com.example.dating.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RestController
 public class main {
     @GetMapping("/main")
     @ResponseBody
     public String index() {
         System.out.println("testINsert tset!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         return "MainTest";
+    }
+
+
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world!";
     }
 }
