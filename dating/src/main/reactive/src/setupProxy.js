8a169b7ac242718def09a96a1e8b6de9f28841d2
JAE-LEE0909 <kkao456178@gmail.com>
2025-12-13 20:58:16 +0900

    프록시 올림
    정상적인 프록시 상태 접근 #으로 인해 이슈 발생

--- dating/src/main/reactive/src/setupProxy.js
+++ dating/src/main/reactive/src/setupProxy.js
@@ -1,13 +1,21 @@
 // src/main/frontend/src/setProxy.js
+//이부분에 정상적인 코드 안들어오면 실행은 되는데 local에서 팅김
 
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
     createProxyMiddleware({
-      target: 'http://localhost:8080',	# 서버 URL or localhost:설정한포트번호
+      target: 'http://localhost:8080/',
       changeOrigin: true,
-      pathFilter: '/main/'
+      pathFilter: '/main'
     })
   );
+  app.use(
+      createProxyMiddleware({
+        target: 'http://localhost:8080/',
+        changeOrigin: true,
+        pathFilter: '/api/hello'
+      })
+    );
 };
